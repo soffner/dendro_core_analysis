97d680ea2ac588b325c04c7ae391cc90e9056cb4
Stella Offner <tg458122@login1.frontera.tacc.utexas.edu>
2023-04-26 09:20:35 -0500

    Make a maximum radius in which to search for index matching.

--- make_leaf_history_fast.py
+++ make_leaf_history_fast.py
@@ -7,15 +7,18 @@ import glob
 import matplotlib
 matplotlib.use('Agg')
 
-dir  = '/scratch3/03532/mgrudic/STARFORGE_RT/production2/M2e3_R3/M2e3_R3_S0_T1_B0.01_Res126_n2_sol0.5_42/output/'
-run = 'M2e3_R3_S0-T1_B0.01_Res126_n2_sol0.5_42'
+# Location of STARFORGE outputs
+dir  = '/scratch/05917/tg852163/GMC_sim/Runs/Physics_ladder/M2e4_C_M_J_RT_W_R30_v1.1_2e7/output/' 
+
+# Run name
+run = 'M2e4_C_M_J_RT_W_R30_v1.1_2e7'
 outdir = './'
 
 res_limit = 1e-3  # Minimum resolution
 
-fns = glob.glob(dir+'snapshot_*0.hdf5') #[0,1,2,3,4,5,6,7,8,9].hdf5')
+fns = glob.glob(dir+'snapshot_*[0,1,2,3,4,5,6,7,8,9].hdf5')
 fns.sort()
 
-create_leaf_history_fast(fns,run,res_limit=res_limit)
+create_leaf_history_fast(fns,run,res_limit=res_limit,search_radius=1.0)
 
 print("Completed history!")
